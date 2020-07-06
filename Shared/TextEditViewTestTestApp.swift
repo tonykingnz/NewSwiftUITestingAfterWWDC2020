456a56181e8d826529033164915b9ee5a01c9ad2
José Antônio Maria de Rezende Silva <joseant.silva@icloud.com>
2020-07-06 17:42:11 -0300

    Initial Commit

---
+++ Shared/TextEditViewTestTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextEditViewTestTestApp.swift
+//  Shared
+//
+//  Created by José Antônio Maria de Rezende Silva on 7/6/20.
+//
+
+import SwiftUI
+
+@main
+struct TextEditViewTestTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
